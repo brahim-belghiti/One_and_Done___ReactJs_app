079a5e2ecc5ed668678a262f5f0dcc77d0431d8f
belghiti-brahim <belghiti.brahim@pm.me>
2022-06-23 10:04:50 +0100

    change_so_the_app_can_accept_a_link_that_doesn't_start_with_https_and_start_with_www

--- src/pages/Fin.js
+++ src/pages/Fin.js
@@ -1,8 +1,6 @@
 
 function Fin(props) {
 
-
-
     return (
 
         <div>
@@ -11,18 +9,18 @@ function Fin(props) {
                 <h1 className="w-10/12 text-8xl font-black">Go and continue doing productive work.</h1>
                 <div className="w-10/12 grid grid-cols-12">
                     <div className="col-start-1 col-end-4">
-                    <p className="text-2xl text-left font-light">this is not anti youtube, this is anti distraction.</p>
+                        <p className="text-2xl text-left font-light">this is not anti youtube, this is anti distraction.</p>
+                    </div>
+                    <div className="col-start-6 col-end-10">
+                        <p className="text-2xl text-left font-light">some tools to helps you avoid distraction and be more intentional with your use of Youtube:</p>
+                        <ul className="text-2xl text-left font-bold">
+                            <li>Blocktube</li>
+                            <li>DF Youtube</li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="col-start-6 col-end-10">
-                    <p className="text-2xl text-left font-light">some tools to helps you avoid distraction and be more intentional with your use of Youtube:</p>
-                    <ul className="text-2xl text-left font-bold">
-                        <li>Blocktube</li>
-                        <li>DF Youtube</li>
-                    </ul>
-                </div>
-            </div>
 
-        </div>
+            </div>
         </div >
     );
 }
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import YouTube from 'react-youtube';
 
 function Video(props) {
 
     const [isItDone, setisItDone] = useState(false);
+    const [haveError, sethaveError] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
     const pathName = location.pathname;
@@ -18,6 +20,11 @@ function Video(props) {
         },
     };
 
+    function onVideoError(e) {
+        alert('make sure to past the correct link');
+        sethaveError(true);
+    }
+
     function onVideoOnEnd(e) {
         // access to player in all event handlers via event.target
         console.log(e.target);
@@ -26,8 +33,12 @@ function Video(props) {
     return (
 
         <div >
+            <div className="flex flex-col items-center w-full pt-4">
+                {haveError ? <Link to="/">  <button className="bg-orange-500 hover:bg-orange-600 text-white font-black text-2xl py-2 px-4 rounded-full h-16 w-full">back to home</button>
+                </Link> : ""}
+            </div>
             <div className="flex items-center justify-center h-screen">
-                <YouTube videoId={videoId} opts={opts} onEnd={onVideoOnEnd} />
+                <YouTube videoId={videoId} opts={opts} onEnd={onVideoOnEnd} onError={onVideoError} />
             </div>
             {isItDone ? navigate("/fin") : console.log("false")}
         </div>
